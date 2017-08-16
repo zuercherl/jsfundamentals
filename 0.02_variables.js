d90ca7d4e1014f3b1cc7622dd657ac98442f1d1d
Leon Zuercher <lwz1150academy@gmail.com>
2017-08-16 14:34:42 -0400

    Added Variables and Conditionals

---
+++ 0.02_variables.js
@@ -0,0 +1,40 @@
+// Variables
+
+var name;
+
+name = "Madyson";
+
+console.log(typeof(name));
+console.log(name);
+
+var lastName = "Ward";
+
+console.log(name.length);
+console.log(name.toUpperCase());
+// Variable data types
+
+// Challenge create 2 variables 'firstName' and 'LastName' and concat them
+// together in a third vairable called 'fullName' and print fullName to the console
+
+var firstName = "Leon";
+var lastName= "Zuercher";
+var fullName= firstName+" "+lastName;
+
+console.log(fullName);
+
+one = 1;
+two = 2;
+three = 3;
+four = 4;
+five = 5;
+
+console.log(one, two, three, four, five);
+
+
+var myNumber = 0;
+myNumber = myNumber +10;
+myNumber = myNumber -1;
+myNumber = myNumber * 9;
+myNumber = myNumber / 7;
+
+console.log(myNumber);
