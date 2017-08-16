485f284b9b245cd4cd78285da34ae58ffc84af49
Leon Zuercher <lwz1150academy@gmail.com>
2017-08-16 13:20:18 -0400

    Added Datatypes

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,37 @@
+//DataTypes
+
+// Strings
+//
+"What a wonderful World";
+'Hello you guys';
+
+"it's a wonderful day";
+
+
+console.log('Thomas said "I think grass is green!"'.toUpperCase());
+
+
+console.log( 'Couldn\'t things get better' .length );
+
+// Challenge create a string that has your name and use the to Lowercase functions 
+// to make sure each character is lower case.
+
+console.log('Leon Zuercher' .toLowerCase());
+//           01234567890
+console.log('jay'[0].toUpperCase());
+console.log('jay'[0].toUpperCase() + 'jay'[1,2] + 'jay'[2]);
+
+console.log('leon'[0].toUpperCase() + 'leon'[1]+ 'leon'[2] + 'leon'[3]);
+
+
+// Challenge uppercase the 'm' in Home and put the string
+// back together using string concatenation.
+
+console.log('home'[0].toUpperCase() + 'home'[1]+ 'home'[2] + 'home'[3]);
+
+console.log(typeof("Amber"));
+
+// Numbers
+
+console.log(typeof(2));
+
