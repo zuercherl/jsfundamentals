0d3fe5d5c26f909d236a399747e8171071c6394f
Leon Zuercher <lwz1150academy@gmail.com>
2017-08-16 15:36:42 -0400

    Added Conditionals

--- 0.03_conditionals.js
+++ 0.03_conditionals.js
@@ -1,2 +1,56 @@
 // Conditionals
 
+
+
+
+
+
+
+
+//var name = "Kenn";
+
+//if ( "Kenn" == name) {
+//	console.log( "Hey your name is " + name);
+//  else {
+//  	console.log("What is your name");
+//  }
+// }
+//  Create and If statement that checks if the users name does not have a capital letter,
+//  Rebuild their name with the capitalized first letter.
+
+
+ var inputName = "Kenn";
+
+console.log( "Hey your nam/e `is " + inputName);   // Get input name
+
+if (inputName[0] != inputName[0].toUpperCase()) {   // Check to see if 1st position is capital or not.
+	console.log("Horrible Punctuation");
+		inputName = inputName = inputName[0].toUpperCase + inputName[1] + inputName[2]+ inputName[3];
+ }
+  else {
+  	console.log("great Punctuation")
+  }
+//}
+
+//           01234567890
+// console.log('jay'[0].toUpperCase());
+
+NOT WORKING 3:30 pm 2017-08-16
+var age = 16
+
+if (age>=18){
+	console.log("Yay, you can vote");
+	Else if(age >= 21) {
+	console.log("Yay, you can drink");	
+	Else if(age >= 18) {
+	console.log("Yay, you can vote");
+		Else if(age >= 21) {
+	console.log("Yay, you can drink");
+	}
+}
+
+
+
+
+
+}
