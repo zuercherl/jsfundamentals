d8eec56dc641bb9490811f44c20934b256668122
Leon Zuercher <lwz1150academy@gmail.com>
2017-08-17 10:57:02 -0400

    Added functions

---
+++ 0.05_functions.js
@@ -0,0 +1,71 @@
+// Functions
+
+
+function greeting() {
+	console.log("Good Morning!");
+}
+
+// parenthesis invokes our funciton so that it runs the code inside of it.
+greeting();
+
+// Challenge, Create a function that says "Day 4 feeling great!" then call that function
+
+// scope
+
+
+// Void Functions
+
+// Functions
+
+// Functions with Paramenters
+
+
+
+
+
+function sum(num1, num2){
+	console.log(num1 + num2)
+}
+
+sum(3, 4)
+
+function feelin_good() {
+	console.log("Day 4 feeling great!");
+}
+
+
+var fname = "Aj";
+
+function hello(fname){
+	var lname = "Hephner"
+	console.log(fname + " " +"lname")
+}
+console.log("lname");
+
+hello("Aj");
+
+if ( 1 ==1 ) {
+//	var sum = 1 + 1		// var is created within code block, but is local
+	let sum = 1 + 1      // Let stays local
+	console.log(sum);
+}
+console.log(sum); //err
+
+
+// Create a function calle capitalize() and use the for loop we created yesterday to
+// capitalize names
+
+var name = "kenn";
+
+function capitalize(inputName){
+	capitalName = inputName[0].toUpperCase() + inputName[1] + inputName[2]+ inputName[3];
+	console.log(capitalName);
+}
+
+capitalize(name);
+
+
+// parenthesis invokes our funciton so that it runs the code inside of it.
+feelin_good();
+
+
